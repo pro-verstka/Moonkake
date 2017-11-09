92f71d04595ef47bf15b7f23973165f8b753f8fe
pro-verstka <pro-verstka@yandex.ru>
2017-11-09 19:36:20 +0400

    - fixes

---
+++ gulpfile.js
@@ -0,0 +1,265 @@
+/* COMMON
+-------------------------------------------------- */
+
+var
+  gulp = require('gulp'),
+
+  // utils
+  browserSync = require('browser-sync'),
+  flatten = require('gulp-flatten'),
+  gutil = require('gulp-util'),
+  merge = require('merge-stream'),
+  rename = require('gulp-rename'),
+  sourcemaps = require('gulp-sourcemaps'),
+
+  // css
+  gcmq = require('gulp-group-css-media-queries'),
+  autoprefixer = require('gulp-autoprefixer'),
+  csscomb = require('gulp-csscomb'),
+  cssnano = require('gulp-cssnano'),
+
+  //less
+  //less = require('gulp-less'),
+  //glob = require('less-plugin-glob'),
+
+  //sass
+  sass = require('gulp-sass'),
+  glob = require('gulp-sass-glob'),
+
+  // js
+  uglify = require('gulp-uglify'),
+  concat = require('gulp-concat'),
+
+  // tpl
+  pug = require('gulp-pug'),
+
+  // sprites
+  spritesmith = require('gulp.spritesmith'),
+
+  // backup
+  revall = require('gulp-rev-all'),
+  archiver = require('gulp-archiver');
+
+/* TASKS
+-------------------------------------------------- */
+
+/* Browser */
+
+gulp.task('browser', function () {
+  return browserSync({
+    server: {
+      baseDir: './dist/',
+      directory: true
+    },
+    notify: false
+  });
+});
+
+/* Templates */
+
+gulp.task('templates', function () {
+  return gulp.src([
+    'src/templates/**/*',
+    '!src/templates/blocks/*'
+  ], {
+    base: '.'
+  })
+  .pipe(pug({
+    pretty: true
+  }))
+  .on('error', gutil.log)
+  .pipe(flatten())
+  .pipe(gulp.dest('dist/'))
+  .pipe(browserSync.reload({
+    stream: true
+  }));
+});
+
+/* PNG Sprites */
+
+gulp.task('sprite', function () {
+  var spriteData = gulp.src('src/sprite/**/*', {
+    base: '.'
+  })
+  .pipe(spritesmith({
+    //retinaSrcFilter: ['src/sprite/**/*@2x.png'],
+    //retinaImgName: 'sprite@2x.png',
+    imgPath: '../img/sprite.png',
+    imgName: 'sprite.png',
+    cssName: 'sprite.sass',
+    padding: 1
+  }));
+
+  var imgStream = spriteData.img.pipe(gulp.dest('src/img/'));
+  var cssStream = spriteData.css.pipe(gulp.dest('src/css/sprite/'));
+
+  return merge(imgStream, cssStream);
+});
+
+/* Styles */
+
+gulp.task('css', function () {
+  // sass
+  return gulp.src('src/css/general.sass', {
+    base: '.'
+  })
+  //.pipe(sourcemaps.init())
+  .pipe(glob())
+  .pipe(sass())
+  .on('error', gutil.log)
+  .pipe(autoprefixer({
+    browsers: ['last 3 version', 'ie >= 11']
+  }))
+  .pipe(gcmq())
+  .pipe(csscomb())
+  .pipe(flatten())
+  .pipe(gulp.dest('dist/assets/css/'))
+  .pipe(rename({
+    suffix: '.min'
+  }))
+  .pipe(cssnano({
+    zindex: false
+  }))
+  //.pipe(sourcemaps.write('maps'))
+  .pipe(gulp.dest('dist/assets/css/'))
+  .pipe(browserSync.reload({
+    stream: true
+  }));
+
+// less
+// return gulp.src('src/assets/css/general.less', {
+//     base: '.'
+//   })
+//   .pipe(sourcemaps.init())
+//   .pipe(less({
+//     plugins: [glob],
+//     compress: false
+//   }))
+//   .pipe(autoprefixer({
+//     browsers: ['last 3 version']
+//   }))
+//   .on('error', gutil.log)
+//   .pipe(gcmq())
+//   .pipe(csscomb())
+//   .pipe(flatten())
+//   .pipe(gulp.dest('dist/assets/css/'))
+//   .pipe(rename({
+//     suffix: '.min'
+//   }))
+//   .pipe(cssnano({
+//     zindex: false
+//   }))
+//   .pipe(sourcemaps.write('maps'))
+//   .pipe(gulp.dest('dist/assets/css/'))
+//   .pipe(browserSync.reload({
+//     stream: true
+//   }));
+});
+
+/* Fonts */
+
+gulp.task('fonts', function () {
+  return gulp.src('src/fonts/**/*', {
+    base: './src/fonts/'
+  })
+  .pipe(gulp.dest('dist/assets/fonts/'))
+  .pipe(browserSync.reload({
+    stream: true
+  }));
+});
+
+/* Images */
+
+gulp.task('img', function () {
+  return gulp.src('src/img/**/*', {
+    base: './src/img/'
+  })
+  .pipe(gulp.dest('dist/assets/img/'))
+  .pipe(browserSync.reload({
+    stream: true
+  }));
+});
+
+/* Scripts */
+gulp.task('js:common', function () {
+  return gulp.src('src/js/common.js', {
+    base: './src/js/'
+  })
+  .pipe(sourcemaps.init())
+  .pipe(gulp.dest('dist/assets/js/'))
+  .pipe(uglify())
+  .pipe(rename({
+    suffix: '.min'
+  }))
+  .pipe(sourcemaps.write('maps'))
+  .pipe(gulp.dest('dist/assets/js/'));
+});
+
+gulp.task('js:bundle', function () {
+  return gulp.src([
+    'src/js/vendor/**/*.js',
+    '!src/js/vendor/**/_*.js'
+  ], {
+    base: '.'
+  })
+  .pipe(sourcemaps.init())
+  .pipe(concat('bundle.min.js'))
+  .pipe(uglify())
+  .pipe(sourcemaps.write('maps'))
+  .pipe(gulp.dest('dist/assets/js/'));
+});
+
+gulp.task('js:all', ['js:bundle', 'js:common']);
+
+/* Backup */
+
+gulp.task('backup', function () {
+  var path = require('path');
+  var package = path.parse(__dirname);
+
+  return gulp.src([
+    'src/**/*',
+    'dist/**/*',
+    '.csscomb.json',
+    '.gitignore',
+    'gulpfile.js',
+    'package.json',
+    'README.md'
+  ], {
+    base: '.'
+  })
+  .pipe(archiver(package.name + '.zip'))
+  .pipe(revall.revision({
+    transformFilename: function (file, hash) {
+      function addZero(number) {
+        return number < 10 ? '0' + number : '' + number;
+      }
+
+      var date = new Date();
+      var day = addZero(date.getDate());
+      var month = addZero(date.getMonth() + 1);
+      var year = date.getFullYear();
+      var hours = addZero(date.getHours());
+      var minutes = addZero(date.getMinutes());
+      return package.name + '.' + year + '.' + month + '.' + day + '_' + hours + '.' + minutes + '.zip';
+    }
+  }))
+  .pipe(gulp.dest('backup/'));
+});
+
+/* Common */
+
+gulp.task('build', ['templates', 'sprite', 'css', 'img', 'fonts', 'js:all'], function () {
+  gutil.log('Project building done!');
+});
+
+gulp.task('default', ['browser'], function () {
+  gulp.watch('src/templates/**/*', ['templates']);
+  gulp.watch('src/sprite/**/*', ['sprite']);
+  gulp.watch('src/css/**/*', ['css']);
+  gulp.watch('src/img/**/*', ['img']);
+  gulp.watch('src/fonts/**/*', ['fonts']);
+  gulp.watch('src/js/**/*', ['js:all']);
+
+  gutil.log('Project is running!');
+});
---
+++ src/js/common.js
@@ -0,0 +1,383 @@
+/*!
+ * Moonkake v5.0.0
+ *
+ * https://github.com/detectiveshelby/moonkake
+ */
+
+$(function () {
+
+  /* VAR
+  -------------------------------------------------- */
+
+  var $d = $(document);
+  var $w = $(window);
+  var $h = $('html');
+  var $b = $('body');
+
+  /* UTILS
+  -------------------------------------------------- */
+
+  var $mk = {
+    scrollTo: function ($object, offset, callback) {
+      $('html, body').stop().animate({
+        scrollTop: $object.offset().top - ((typeof (offset) == 'number') ? offset : 0)
+      }, 500, function () {
+        if ($(this)[0].nodeName == 'HTML') {
+          if (typeof (callback) == 'function') {
+            callback();
+          }
+        }
+      });
+    },
+
+    getScreenIndex: function ($object, offset) {
+      var index = 0;
+
+      $object.each(function () {
+        var $self = $(this);
+
+        if ($d.scrollTop() >= $self.offset().top - offset) {
+          index = $self.index();
+        }
+      });
+
+      return index;
+    },
+
+    numberFormat: function (number, decimals, dec_point, thousands_sep) {
+      // original by: Jonas Raoni Soares Silva (http://www.jsfromhell.com)
+      // improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+      // bugfix by: Michael White (http://crestidg.com)
+      var i, j, kw, kd, km;
+
+      if (isNaN(decimals = Math.abs(decimals))) {
+        decimals = 2;
+      }
+      if (dec_point == undefined) {
+        dec_point = ',';
+      }
+      if (thousands_sep == undefined) {
+        thousands_sep = '.';
+      }
+
+      i = parseInt(number = (+number || 0).toFixed(decimals)) + '';
+
+      if ((j = i.length) > 3) {
+        j = j % 3;
+      } else {
+        j = 0;
+      }
+
+      km = j ?
+        i.substr(0, j) + thousands_sep :
+        '';
+      kw = i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousands_sep);
+      kd = (decimals ?
+        dec_point + Math.abs(number - i).toFixed(decimals).replace(/-/, '0').slice(2) :
+        '');
+
+      return km + kw + kd;
+    },
+  };
+
+  /* SCROLL
+  -------------------------------------------------- */
+
+  $('.js-scroll').on('click', function () {
+    var href = $(this).attr('href');
+
+    $mk.scrollTo($(href), 0);
+
+    return false;
+  });
+
+  /* POPUP
+  -------------------------------------------------- */
+
+  $('.js-popup').magnificPopup({
+    type: 'inline',
+    midClick: true,
+    closeBtnInside: true,
+    removalDelay: 300,
+    mainClass: 'mfp-fade',
+    fixedContentPos: false,
+    callbacks: {
+      beforeOpen: function() {
+        $d.trigger('mfpPopupBeforeOpen', {
+          instance: this
+        });
+      },
+      open: function() {
+        $d.trigger('mfpPopupOpen', {
+          instance: this
+        });
+      },
+      beforeClose: function() {
+        $d.trigger('mfpPopupBeforeClose', {
+          instance: this
+        });
+      },
+      close: function() {
+        $d.trigger('mfpPopupClose', {
+          instance: this
+        });
+      },
+      afterClose: function() {
+        $d.trigger('mfpPopupAfterClose', {
+          instance: this
+        });
+      }
+    }
+  });
+
+  $('.js-image').magnificPopup({
+    type: 'image',
+    closeOnContentClick: true,
+    removalDelay: 300,
+    mainClass: 'mfp-fade mfp-img-mobile',
+    image: {
+      verticalFit: true
+    },
+    callbacks: {
+      beforeOpen: function() {
+        $d.trigger('mfpImageBeforeOpen', {
+          instance: this
+        });
+      },
+      open: function() {
+        $d.trigger('mfpImageOpen', {
+          instance: this
+        });
+      },
+      beforeClose: function() {
+        $d.trigger('mfpImageBeforeClose', {
+          instance: this
+        });
+      },
+      close: function() {
+        $d.trigger('mfpImageClose', {
+          instance: this
+        });
+      },
+      afterClose: function() {
+        $d.trigger('mfpImageAfterClose', {
+          instance: this
+        });
+      }
+    }
+  });
+
+  $('.js-gallery').each(function() {
+    $(this).magnificPopup({
+      delegate: 'a',
+      type: 'image',
+      tLoading: 'Загрузка изображения #%curr%...',
+      mainClass: 'mfp-fade mfp-img-mobile',
+      removalDelay: 300,
+      gallery: {
+        enabled: true,
+        navigateByImgClick: true,
+        preload: [0, 1],
+        tCounter: '<span class="mfp-counter">%curr% из %total%</span>'
+      },
+      image: {
+        tError: '<a href="%url%">Изображение #%curr%</a> не может быть загружено.'
+      },
+      callbacks: {
+        beforeOpen: function() {
+          $d.trigger('mfpGalleryBeforeOpen', {
+            instance: this
+          });
+        },
+        open: function() {
+          $d.trigger('mfpGalleryOpen', {
+            instance: this
+          });
+        },
+        beforeClose: function() {
+          $d.trigger('mfpGalleryBeforeClose', {
+            instance: this
+          });
+        },
+        close: function() {
+          $d.trigger('mfpGalleryClose', {
+            instance: this
+          });
+        },
+        afterClose: function() {
+          $d.trigger('mfpGalleryAfterClose', {
+            instance: this
+          });
+        }
+      }
+    });
+  });
+
+  /* TABS
+  -------------------------------------------------- */
+
+  var $tabs = {
+    opts: {
+      root: '.tabs:not(.tabs-nojs)',
+      title: '.tabs-title',
+      content: '.tabs-content',
+      item: '.tabs-item',
+      active: '-active'
+    },
+
+    changeTab: function ($root, index) {
+      var opt = this.opts;
+
+      $root.find(opt.title + ' ' + opt.item).eq(index).addClass(opt.active).siblings(opt.item).removeClass(opt.active);
+      $root.find(opt.content + ' ' + opt.item).eq(index).addClass(opt.active).siblings(opt.item).removeClass(opt.active);
+
+      $d.trigger('tabChanged', {
+        root: $root,
+        index: index
+      });
+    },
+
+    init: function () {
+      var $this = this;
+      var opt = $this.opts;
+
+      $(opt.title).on('click', opt.item, function () {
+        var _ = $(this);
+        var $root = _.closest(opt.root);
+        var index = _.index();
+
+        $this.changeTab($root, index);
+      });
+    }
+  };
+
+  $tabs.init();
+
+  /* CAROUSEL
+  -------------------------------------------------- */
+
+  $('.owl-carousel').owlCarousel({
+    items: 3,
+    loop: true,
+    margin: 0,
+    nav: true,
+    dots: true,
+    autoplay: true,
+    autoplayTimeout: 5000
+  });
+
+  /* COUNT
+  -------------------------------------------------- */
+
+  $('.count').on('click', '.count-plus, .count-minus', function (event) {
+    var _ = $(this),
+      $input = _.closest('.count').find('input'),
+      value = parseInt($input.val()),
+      min = $input.attr('min') || '',
+      max = $input.attr('max') || '';
+
+    if (event.currentTarget.className == 'count-minus') {
+      value -= 1;
+      if (value <= 1) value = 1;
+    }
+
+    if (event.currentTarget.className == 'count-plus') {
+      value += 1;
+    }
+
+    $input.val(value);
+
+    $d.trigger('inputCountChanged', {
+      input: $input,
+      value: value
+    });
+
+    return false;
+  });
+
+  $('.count').on('focusout', 'input', function (event) {
+    var _ = $(this);
+
+    if (_.val() === '' || _.val() === '0') {
+      _.val(1);
+    }
+  });
+
+  /* NAVIGATION
+  -------------------------------------------------- */
+
+  var $toggler = {
+    options: {
+      toggler: 'data-toggler',
+      target: 'data-target',
+      active: '-active'
+    },
+
+    init: function ($options) {
+      var $this = this;
+
+      if (typeof $options == 'object') {
+        $this.options = $options;
+      }
+
+      $('[' + $this.options.toggler + ']').on('click', function () {
+        var _ = $(this);
+        var target = _.attr($this.options.target);
+
+        $('[' + $this.options.target + ']').each(function () {
+          var _ = $(this);
+          var currentTarget = _.attr($this.options.target);
+
+          if (currentTarget !== target) {
+            _.removeClass($this.options.active);
+            $('#' + currentTarget).removeClass($this.options.active);
+          }
+        });
+
+        _.toggleClass($this.options.active);
+
+        $('#' + target).toggleClass($this.options.active);
+
+        return false;
+      });
+
+      $d.on('click', function () {
+        $('[' + $this.options.toggler + ']').removeClass($this.options.active);
+
+        $('[' + $this.options.toggler + ']').each(function () {
+          var _ = $(this);
+          var target = _.attr($this.options.target);
+
+          _.removeClass($this.options.active);
+          $('#' + target).removeClass($this.options.active);
+        });
+      });
+
+      $('[' + $this.options.toggler + ']').on('click', function (event) {
+        event.stopPropagation();
+      });
+
+      $('[' + $this.options.toggler + ']').each(function() {
+        var _ = $(this);
+        var target = _.attr($this.options.target);
+
+        $('#' + target).each(function() {
+          $(this).on('click', function (event) {
+            event.stopPropagation();
+          });
+        });
+      });
+
+    }
+  }
+
+  $toggler.init();
+
+  /* INPUTMASK
+  -------------------------------------------------- */
+
+  $('input[type="tel"]').inputmask({
+    mask: '+7 (999) 999-99-99'
+  });
+
+});

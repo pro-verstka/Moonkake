1065347ffb5ac088884418a9800fadb1fc1196bd
pro-verstka <pro-verstka@yandex.ru>
2018-03-18 19:49:18 +0400

    add vue.js

---
+++ src/js/app.js
@@ -0,0 +1,22 @@
+/* APP
+-------------------------------------------------- */
+
+var app = new Vue({
+	el: '#app',
+
+	data: {
+		message: 'Moonkake'
+	},
+
+	computed: {
+
+	},
+
+	methods: {
+
+	},
+
+	mounted: function() {
+		console.log('mounted');
+	}
+});

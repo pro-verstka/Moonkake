bd47a10c3228b14b733ce27e22482a9d620de36e
Andrew <pro-verstka@yandex.ru>
2019-07-11 11:49:59 +0400

    pdopage fix

--- src/js/components/lazyload.js
+++ src/js/components/lazyload.js
@@ -6,3 +6,12 @@ const lazyLoadInstance = new LazyLoad({
 	elements_selector: 'img:not(.swiper-lazy)',
 	use_native: true
 })
+
+// if (typeof pdoPage === 'object') {
+// 	pdoPage.callbacks['after'] = (config, response) => {
+// 		document.querySelector(config.wrapper).removeAttribute('style')
+// 		document.querySelector(config.wrapper).classList.remove('loading')
+
+// 		lazyLoadInstance.update()
+// 	}
+// }

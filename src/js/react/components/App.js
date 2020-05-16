3bbdaec29a8bed2ede4aaa51956557ace5d58d0c
p1rate <pro-verstka@yandex.ru>
2020-05-16 09:24:23 +0400

    fix

--- src/js/react/components/App.js
+++ src/js/react/components/App.js
@@ -1,14 +1,9 @@
 import React, { useState } from 'react'
 
 const App = () => {
-
 	const [name, setName] = useState('React')
 
-	return (
-		<div>
-			Hello from { name }!
-		</div>
-	)
+	return <div>Hello from {name}!</div>
 }
 
 export default App

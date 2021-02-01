4adb043a3174196b71f44ab0cdb4dd2e45017b2a
p1rate <pro-verstka@yandex.ru>
2021-02-01 19:22:02 +0400

    Fix gallery.js & polyfills.js

--- src/js/components/gallery.js
+++ src/js/components/gallery.js
@@ -4,40 +4,40 @@ import PhotoSwipeUI from 'photoswipe/dist/photoswipe-ui-default'
 document.body.insertAdjacentHTML(
 	'beforeend',
 	`
-	<div class="pswp">
-		<div class="pswp__bg"></div>
-		<div class="pswp__scroll-wrap">
-			<div class="pswp__container">
-				<div class="pswp__item"></div>
-				<div class="pswp__item"></div>
-				<div class="pswp__item"></div>
-			</div>
-			<div class="pswp__ui pswp__ui--hidden">
-				<div class="pswp__top-bar">
-					<div class="pswp__counter"></div>
-					<button class="pswp__button pswp__button--close" />
-					<button class="pswp__button pswp__button--fs" />
-					<button class="pswp__button pswp__button--zoom" />
-					<div class="pswp__preloader">
-						<div class="pswp__preloader__icn">
-							<div class="pswp__preloader__cut">
-								<div class="pswp__preloader__donut"></div>
+		<div class="pswp" tabindex="-1" role="dialog" aria-hidden="true">
+			<div class="pswp__bg"></div>
+			<div class="pswp__scroll-wrap">
+				<div class="pswp__container">
+					<div class="pswp__item"></div>
+					<div class="pswp__item"></div>
+					<div class="pswp__item"></div>
+				</div>
+				<div class="pswp__ui pswp__ui--hidden">
+					<div class="pswp__top-bar">
+						<div class="pswp__counter"></div>
+						<button class="pswp__button pswp__button--close" title="Close (Esc)"></button>
+						<button class="pswp__button pswp__button--fs" title="Toggle fullscreen"></button>
+						<button class="pswp__button pswp__button--zoom" title="Zoom in/out"></button>
+						<div class="pswp__preloader">
+							<div class="pswp__preloader__icn">
+								<div class="pswp__preloader__cut">
+									<div class="pswp__preloader__donut"></div>
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-				<div class="pswp__share-modal pswp__share-modal--hidden pswp__single-tap">
-					<div class="pswp__share-tooltip"></div>
-				</div>
-				<button class="pswp__button pswp__button--arrow--left" />
-				<button class="pswp__button pswp__button--arrow--right" />
-				<div class="pswp__caption">
-					<div class="pswp__caption__center"></div>
+					<div class="pswp__share-modal pswp__share-modal--hidden pswp__single-tap">
+						<div class="pswp__share-tooltip"></div> 
+					</div>
+					<button class="pswp__button pswp__button--arrow--left" title="Previous (arrow left)"></button>
+					<button class="pswp__button pswp__button--arrow--right" title="Next (arrow right)"></button>
+					<div class="pswp__caption">
+						<div class="pswp__caption__center"></div>
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-`
+	`
 )
 
 const pswpElement = document.querySelector('.pswp')
--- src/js/vendor/polyfills.js
+++ src/js/vendor/polyfills.js
@@ -2,7 +2,6 @@ import 'core-js/features/array/from'
 import 'core-js/features/array/find'
 import 'core-js/features/array/find-index'
 import 'core-js/features/array/includes'
-import 'core-js/features/symbol/index'
 import 'core-js/features/symbol/iterator'
 import 'core-js/features/object/assign'
 import 'core-js/features/object/keys'
@@ -11,10 +10,10 @@ import 'element-closest-polyfill'
 import 'nodelist-foreach-polyfill'
 import 'custom-event-polyfill'
 
-Element.prototype.remove = function() {
+Element.prototype.remove = function () {
 	this.parentElement.removeChild(this)
 }
-NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
+NodeList.prototype.remove = HTMLCollection.prototype.remove = function () {
 	for (var i = this.length - 1; i >= 0; i--) {
 		if (this[i] && this[i].parentElement) {
 			this[i].parentElement.removeChild(this[i])

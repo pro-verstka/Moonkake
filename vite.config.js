b5ca38c516ce119732bcd64e9b9a07882e7cf882
pro-verstka <pro-verstka@yandex.ru>
2024-03-18 19:35:02 +0400

    Generate index page

--- vite.config.js
+++ vite.config.js
@@ -9,21 +9,44 @@ const PUBLIC_DIR = 'assets'
 const getEntries = () => {
 	return fs
 		.readdirSync(path.resolve(__dirname, ROOT_DIR))
-		.filter(f => f.endsWith('.pug'))
-		.map(tpl => `${ROOT_DIR}/${tpl}.html`)
+		.filter(file => file.endsWith('.pug'))
 }
+
+const getInput = entries => {
+	return entries.map(tpl => `${ROOT_DIR}/${tpl}.html`)
+}
+
+const generateIndexPage = entries => {
+	const INDEX_PAGE = 'index.pug'
+	let content = `doctype html\nhtml(lang="ru")\n\thead\n\t\tmeta(charset="utf-8")\n\tbody\n\t\tol`
+
+	for (const tpl of entries) {
+		if (tpl === INDEX_PAGE) continue
+
+		const [name] = tpl.split('.')
+		content += `\n\t\t\tli: a(href="${name}.html") ${name}`
+	}
+
+	fs.writeFileSync(`${ROOT_DIR}/${INDEX_PAGE}`, content)
+}
+
+const entries = getEntries()
+const input = getInput(entries)
+generateIndexPage(entries)
 
 export default defineConfig({
 	base: './',
 	root: ROOT_DIR,
 	publicDir: `${ROOT_DIR}/${PUBLIC_DIR}`,
-	plugins: [pug({
-		root: ROOT_DIR,
-		data: path.resolve(__dirname, `${ROOT_DIR}/data/*.json`),
-		options: {
-			pretty: true
-		}
-	})],
+	plugins: [
+		pug({
+			root: ROOT_DIR,
+			data: path.resolve(__dirname, `${ROOT_DIR}/data/*.json`),
+			options: {
+				pretty: true,
+			},
+		}),
+	],
 	build: {
 		target: 'modules',
 		assetsDir: PUBLIC_DIR,
@@ -31,35 +54,33 @@ export default defineConfig({
 		emptyOutDir: true,
 		cssMinify: 'lightningcss',
 		rollupOptions: {
-			input: getEntries(),
+			input,
 			output: {
 				dir: './dist',
-				entryFileNames: (chunk) => {
+				chunkFileNames: `${PUBLIC_DIR}/js/[name].js`,
+
+				entryFileNames: chunk => {
 					const name = chunk.name.replace(/\.pug$/, '')
 
 					return `${PUBLIC_DIR}/js/${name}.js`
 				},
-				chunkFileNames: `${PUBLIC_DIR}/js/[name].js`,
-				assetFileNames: (chunk) => {
+
+				assetFileNames: chunk => {
 					const extname = path.extname(chunk.name)
+					let type = 'media'
 
-					if (['.css'].includes(extname)) {
-						return `${PUBLIC_DIR}/css/${chunk.name}`
-					}
+					if (['.css'].includes(extname)) type = 'css'
+					if (['.woff', '.woff2'].includes(extname)) type = 'fonts'
 
-					if (['.woff', '.woff2'].includes(extname)) {
-						return `${PUBLIC_DIR}/fonts/${chunk.name}`
-					}
-
-					return `${PUBLIC_DIR}/media/${chunk.name}`
-				}
-			}
+					return `${PUBLIC_DIR}/${type}/${chunk.name}`
+				},
+			},
 		},
 	},
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, ROOT_DIR),
 			'@@': path.resolve(__dirname, './node_modules'),
-		}
+		},
 	},
 })

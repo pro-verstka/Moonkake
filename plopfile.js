99fdd12add8f652c002eac028fb63064b02b5563
pro-verstka <pro-verstka@yandex.ru>
2025-04-16 22:33:53 +0400

    feat: twig support

--- plopfile.js
+++ plopfile.js
@@ -4,7 +4,7 @@
  */
 export default function (plop) {
 	plop.setGenerator('page', {
-		description: 'Create new page with scripts and styles',
+		description: 'Create new page with scripts and styles (Pug)',
 		prompts: [
 			{
 				type: 'input',
@@ -55,6 +55,83 @@ ${
 }
 
 block main`,
+			})
+
+			if (isJS) {
+				actions.push({
+					type: 'add',
+					path: 'src/scripts/pages/{{name}}.js',
+					template: `console.log('hello from {{name}}.js')`,
+				})
+			}
+
+			if (isCSS) {
+				actions.push({
+					type: 'add',
+					path: 'src/styles/pages/{{name}}.scss',
+					template: `@use '../mixins';`,
+				})
+			}
+
+			return actions
+		},
+	})
+
+	plop.setGenerator('page-twig', {
+		description: 'Create new page with scripts and styles (Twig)',
+		prompts: [
+			{
+				type: 'input',
+				name: 'name',
+				message: 'Enter page name:',
+			},
+			{
+				type: 'checkbox',
+				name: 'assets',
+				message: 'Add css and/or js for page?',
+				choices: [
+					{
+						name: 'CSS',
+						value: 'css',
+						checked: true,
+					},
+					{
+						name: 'JS',
+						value: 'js',
+						checked: true,
+					},
+				],
+			},
+		],
+		actions: data => {
+			const actions = []
+
+			const isCSS = data.assets.includes('css')
+			const isJS = data.assets.includes('js')
+
+			actions.push({
+				type: 'add',
+				path: 'src/{{name}}.twig',
+				template: `{% extends 'templates-twig/layouts/layout.twig' %}
+
+${
+	isCSS
+		? `{% block css %}
+    <link rel="stylesheet" href="/styles/pages/{{name}}.scss">
+{% endblock %}`
+		: ''
+}
+
+${
+	isJS
+		? `{% block js %}
+    <script type="module" src="/scripts/pages/{{name}}.js"></script>
+{% endblock %}`
+		: ''
+}
+
+{% block main %}
+{% endblock %}`,
 			})
 
 			if (isJS) {
